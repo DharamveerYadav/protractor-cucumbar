1cd8f55bc39020286548d969f1763f5c96ee9c7c
Yadav <dyadav.contractor@rti.org>
2020-04-03 08:33:12 +0530

    code added

--- Test/steps/config.js
+++ Test/steps/config.js
@@ -2,12 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var CucumberReportExtension_1 = require("../reporting/CucumberReportExtension");
 var jsonReports = process.cwd() + "/reports/json";
+process.env.CHROME_BIN = process.env.CHROME_BIN || require("puppeteer").executablePath();
 exports.config = {
     // if using seleniumServerJar, do not specify seleniumAddress !!!
     //seleniumAddress: 'http://localhost:4444/wd/hub',
     directConnect: true,
     SELENIUM_PROMISE_MANAGER: false,
     specs: ["../features/*.feature"],
+    capabilities: {
+        chromeOptions: {
+          args: ["--headless", "--disable-gpu", "--window-size=1200,900"],
+          binary: process.env.CHROME_BIN
+        },
+        'browserName': 'chrome'
+      },
+      directConnect: true,
     framework: 'custom',
     frameworkPath: require.resolve('protractor-cucumber-framework'),
     baseUrl: "http://localhost:8808/",
